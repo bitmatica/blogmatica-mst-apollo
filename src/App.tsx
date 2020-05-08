7ae34a2826d28a37bb2b25ef986e1ace2f972689
Kasey Moffat <kasey@bitmatica.com>
2020-05-08 16:13:06 -0700

    poc

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { BrowserRouter, Switch } from "react-router-dom";
 import { ApolloProvider } from "@apollo/react-hooks";
 import getApolloClient from "./getApolloClient";
 import theme from "./theme";
-import { ThemeProvider, CSSReset } from "@chakra-ui/core";
+import { CSSReset, ThemeProvider } from "@chakra-ui/core";
 import { StoreProvider, useStore } from "./store/RootStore";
 import PrivateRoute from "./components/common/PrivateRoute";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import User from "./pages/User";
+import OAuthApps from "./pages/OAuthApps";
+import GustoAPI from "./pages/GustoAPI";
 import OnlyLoggedOutRoute from "./components/common/OnlyLoggedOutRoute";
 import RegisterUser from "./pages/RegisterUser";
 
@@ -29,6 +31,8 @@ const App: React.FunctionComponent = () => {
               <OnlyLoggedOutRoute path="/login" component={Login} />
               <OnlyLoggedOutRoute path="/register" component={RegisterUser} />
               <PrivateRoute path={"/user/:userId"} component={User} />
+              <PrivateRoute path={"/apps/connect"} component={OAuthApps} />
+            <PrivateRoute path={"/gusto/*"} component={GustoAPI} />
               <PrivateRoute path="*" component={Home} />
             </Switch>
           </ApolloProvider>
--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,5 @@
 export const APP_NAME = "todoapp";
-export const SERVER_URI = "http://localhost:3001/graphql";
+export const SERVER_URI = "https://gusto.apps.bitmatica.com/graphql";
+export const SERVER_REST_URI = "https://gusto.apps.bitmatica.com"
+// export const SERVER_REST_URI = "localhost:3001"
 export const JWT_SESSION_STORAGE_KEY = `${APP_NAME}_jwt`;
