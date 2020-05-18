7482ed8f19c4f90fcdd6a2a3a2e6af81dfa41c48
Zac Palin <zpalin@gmail.com>
2020-05-18 06:21:57 -0700

    Makes tables scroll horizontally on overflow

--- src/pages/admin/ModelListPage.tsx
+++ src/pages/admin/ModelListPage.tsx
@@ -1,3 +1,4 @@
+import { Box } from "@chakra-ui/core"
 import { observer } from "mobx-react-lite"
 import { IModelType } from "mobx-state-tree"
 import pluralize from "pluralize"
@@ -34,34 +35,36 @@ const ModelListPage: React.FC<ModelListPageProps> = ({ model }) => {
   const records = getModelListData(model, data)
   const fields = getModelListFields(model)
   return (
-    <Card m="8">
+    <Card m={[0, 4, 8]}>
       <Card.Header>
         <Card.Header.Title>{pluralize(model.name)}</Card.Header.Title>
         <Card.Header.Text>Listing all {pluralizeModel(model)}</Card.Header.Text>
       </Card.Header>
 
       <LoadingContainer loading={loading} my="8">
-        <Table>
-          <Table.Head>
-            <Table.Head.Row>
-              {fields.map((field) => (
-                <Table.Head.Cell key={field.name}>{field.label}</Table.Head.Cell>
-              ))}
-            </Table.Head.Row>
-          </Table.Head>
-
-          <Table.Body>
-            {records.map((record) => (
-              <Table.Body.Row key={record.id}>
+        <Box overflowX="scroll">
+          <Table>
+            <Table.Head>
+              <Table.Head.Row>
                 {fields.map((field) => (
-                  <Table.Body.Cell key={field.name}>
-                    {formatModelField(model, record, field)}
-                  </Table.Body.Cell>
+                  <Table.Head.Cell key={field.name}>{field.label}</Table.Head.Cell>
                 ))}
-              </Table.Body.Row>
-            ))}
-          </Table.Body>
-        </Table>
+              </Table.Head.Row>
+            </Table.Head>
+
+            <Table.Body>
+              {records.map((record) => (
+                <Table.Body.Row key={record.id}>
+                  {fields.map((field) => (
+                    <Table.Body.Cell key={field.name}>
+                      {formatModelField(model, record, field)}
+                    </Table.Body.Cell>
+                  ))}
+                </Table.Body.Row>
+              ))}
+            </Table.Body>
+          </Table>
+        </Box>
       </LoadingContainer>
     </Card>
   )
