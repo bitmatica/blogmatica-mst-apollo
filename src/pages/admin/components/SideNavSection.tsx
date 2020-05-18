6eb652c6a8b1fc28cefdbdba77afc7d2d0e6efe2
Zac Palin <zpalin@gmail.com>
2020-05-18 09:11:43 -0700

    useDisclosure instead of setState

--- src/pages/admin/components/SideNavSection.tsx
+++ src/pages/admin/components/SideNavSection.tsx
@@ -1,4 +1,4 @@
-import { Box, BoxProps, Collapse, Flex, Icon, Text } from "@chakra-ui/core"
+import { Box, BoxProps, Collapse, Flex, Icon, Text, useDisclosure } from "@chakra-ui/core"
 import React from "react"
 
 type SideNavSectionProps = BoxProps & {
@@ -10,13 +10,12 @@ export const SideNavSection: React.FC<SideNavSectionProps> = ({
   children,
   ...props
 }) => {
-  const [show, setShow] = React.useState(true)
-  const handleToggle = (): void => setShow(!show)
+  const { isOpen, onToggle } = useDisclosure(true)
 
   return (
     <Box borderBottomWidth="1px" borderColor="whiteAlpha.400" {...props}>
       <Flex
-        onClick={handleToggle}
+        onClick={onToggle}
         p="4"
         color="whiteAlpha.600"
         fontWeight="bold"
@@ -27,9 +26,9 @@ export const SideNavSection: React.FC<SideNavSectionProps> = ({
         <Text textTransform="uppercase" width="100%">
           {title}
         </Text>
-        <Icon name={show ? "chevron-up" : "chevron-down"} size="5" />
+        <Icon name={isOpen ? "chevron-up" : "chevron-down"} size="5" />
       </Flex>
-      <Collapse isOpen={show} pb="4">
+      <Collapse isOpen={isOpen} pb="4">
         {children}
       </Collapse>
     </Box>
